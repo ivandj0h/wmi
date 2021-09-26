3aff625da838db96dd8fc3e0678bb1c444dd5817
ivandi1980 <ivandi.djoh@gmail.com>
2021-09-27 03:47:36 +0800

    finalie the python code

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## WMI
 
 ### About
+
 WMI Module to get System Information using Python
+
+### Screen Captured
+
+![Dashboard](dashboard.png "This is the results of Program captured")
--- main.py
+++ main.py
@@ -6,4 +6,11 @@ import wmi
 c = wmi.WMI()
 my_system = c.Win32_ComputerSystem()[0]
 
-print(f"Manufacture: {my_system.Manufacture}")
+print("My Computer's Infos")
+print("-------------------")
+print(f"Manufacture: {my_system.Manufacturer}")
+print(f"Model: {my_system.Model}")
+print(f"Name: {my_system.Name}")
+print(f"Number Of Processors: {my_system.NumberOfProcessors}")
+print(f"System Type: {my_system.SystemType}")
+print(f"System Family: {my_system.SystemFamily}")
