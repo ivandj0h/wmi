1409a054469a836b179b303f83df15198c6e7a57
ivandi1980 <ivandi.djoh@gmail.com>
2021-09-27 03:31:01 +0800

    updates

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # Windows Management Instrumentation
 # To get System Information
 
+import wmi
+
+c = wmi.WMI()
+my_system = c.Win32_ComputerSystem()[0]
+
+print(f"Manufacture: {my_system.Manufacture}")
